798c2eb6c7ded78c3b14510e93532884a5a98f13
Harry <yushuhang@163.com>
2025-02-11 20:56:12 +0800

    新增字体大小适配，图标适配
    图标适配尚待改进，字体基本没问题

--- Assets/Scripts/UI/UIElementMobileCompat.cs
+++ Assets/Scripts/UI/UIElementMobileCompat.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using System.Linq;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using TMPro;
+
 
 [System.Serializable]
 public class UIElementOnlyWidth
@@ -25,11 +27,26 @@ public class UIElementSpaceBetween
     public float originalItemWidth; // 原始组件的宽度
 }
 
+[System.Serializable]
+public class UIElementFontSize
+{
+    public Transform element; // UI 元素
+}
+
+[System.Serializable]
+public class UIElementIconSize
+{
+    public Transform element; // UI 元素
+    public float iconScaleFactor; // 缩放因子
+}
+
 public class UIElementMobileCompat : MonoBehaviour
 {
     public List<UIElementOnlyWidth> onlyWidth = new List<UIElementOnlyWidth>(); // onlyWidth, Inspector 可配置
     public List<UIElementFullScreenWidth> fullScreenWidth = new List<UIElementFullScreenWidth>(); // fullScreenWidth, Inspector 可配置
     public List<UIElementSpaceBetween> spaceBetween = new List<UIElementSpaceBetween>(); // Inspector 可配置
+    public List<UIElementFontSize> fontSize = new List<UIElementFontSize>(); // 字号, Inspector 可配置
+    public List<UIElementIconSize> iconSize = new List<UIElementIconSize>(); // 图片, Inspector 可配置
 
     private float worldScreenWidth;
 
@@ -45,6 +62,8 @@ public class UIElementMobileCompat : MonoBehaviour
         AdjustOnlyWidthElements();
         AdjustFullScreenWidthElements();
         AdjustSpaceBetweenElements();
+        AdjustFontSizeElements();
+        AdjustIconSizeElements();
     }
 
     private void ComputeWorldScreenWidth()
@@ -127,6 +146,73 @@ public class UIElementMobileCompat : MonoBehaviour
             else
             {
                 Debug.LogError("Element does not have a HorizontalLayoutGroup component!");
+            }
+        }
+    }
+
+    /// <summary>
+    /// 计算缩放因子
+    /// </summary>
+    private float CalculateScaleFactor(float scaleFactorMultiplier = 0.5f)
+    {
+        float referenceScreenWidth = 1080f;
+        float referenceScreenHeight = 2408f;
+
+        float widthFactor = Screen.width / referenceScreenWidth;
+        float heightFactor = Screen.height / referenceScreenHeight;
+
+        return 1 + (widthFactor * heightFactor - 1) * scaleFactorMultiplier;
+    }
+
+    /// <summary>
+    /// 调整字体大小
+    /// </summary>
+    private void AdjustFontSizeElements()
+    {
+        float scaleFactor = CalculateScaleFactor(0.5f);
+
+        foreach (var elementData in fontSize)
+        {
+            if (elementData.element == null) continue;
+
+            Text textComponent = elementData.element.GetComponent<Text>();
+            if (textComponent != null)
+            {
+                textComponent.fontSize = Mathf.RoundToInt(textComponent.fontSize * scaleFactor);
+            }
+            else
+            {
+                TMP_Text tmpTextComponent = elementData.element.GetComponent<TMP_Text>();
+                if (tmpTextComponent != null)
+                {
+                    tmpTextComponent.fontSize = tmpTextComponent.fontSize * scaleFactor;
+                }
+                else
+                {
+                    Debug.LogWarning($"{elementData.element.name} does not have a Text or TMP_Text component.");
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 调整图标大小
+    /// </summary>
+    private void AdjustIconSizeElements()
+    {
+        foreach (var elementData in iconSize)
+        {
+            if (elementData.element == null) continue;
+            float scaleFactor = CalculateScaleFactor(elementData.iconScaleFactor);
+
+            RectTransform rt = elementData.element.GetComponent<RectTransform>();
+            if (rt != null)
+            {
+                rt.sizeDelta = new Vector2(rt.sizeDelta.x * scaleFactor, rt.sizeDelta.y * scaleFactor);
+            }
+            else
+            {
+                Debug.LogWarning($"{elementData.element.name} does not have a RectTransform component.");
             }
         }
     }
